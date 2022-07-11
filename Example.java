0d4ae1e48c25c5a68fe2e3e212ee84c6660cc5ab
Ritika <riti.ritikasharma02@gmail.com>
2022-07-11 12:07:08 +0530

    new java example file

---
+++ Example.java
@@ -0,0 +1,17 @@
+ 
+public class Example {
+ 
+    public static void main(String[] args) {
+        int n = 10;
+        int sum=0;
+         
+        int i=1;
+        while (i=n) {
+            sum += i;
+            i++;
+        }
+             
+        System.out.println(sum);
+    }
+     
+}
